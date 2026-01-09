689cbbe657b1a622730d15c30a11f8c89baa1168
Jesus Capetillo <jesscvdevops@gmail.com>
2026-01-09 15:35:58 -0600

    Add: New fixes improve and test client frontend

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + Soy el Admin ALV!!! </h1>
+      <h1>Vite + React + Hello again everyone!!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
